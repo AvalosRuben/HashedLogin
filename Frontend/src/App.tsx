f0ec6e06fa0d291d6a97f3c57db5efd7b9858b6f
AvalosRuben <rubenavalosmtz@gmail.com>
2026-05-14 12:11:35 -0600

    Card

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,5 +1,12 @@
 function App() {
-  return <div>Hola papus</div>;
+  return (
+    <div className="w-screen h-screen bg-gray-200 flex justify-center items-center">
+      {/*Card*/}
+      <div className="w-3xl h-3xl border border-gray-300 rounded-xl bg-white flex flex-col justify-center items-center">
+        Hola papus
+      </div>
+    </div>
+  );
 }
 
 export default App;
