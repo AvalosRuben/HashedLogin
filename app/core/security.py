338dd7d99451f5e57f8d0da6f13bf8dc401fc3bb
G2Titan <93502813+G2Titan@users.noreply.github.com>
2026-05-13 22:19:23 -0600

    HashingArgon2

--- app/core/security.py
+++ app/core/security.py
@@ -4,3 +4,11 @@ password_hash = PasswordHash.recommended()
 
 # Dummy hash 
 DUMMY_HASH = password_hash.hash("bolitasdemar")
+
+# Hash con Argon2
+def hash_password(password: str) -> str:
+    return password_hash.hash(password)
+
+# Verificar contraseña
+def verify_password(password: str, hashed_password: str) -> bool:
+    return password_hash.verify(password, hashed_password)
