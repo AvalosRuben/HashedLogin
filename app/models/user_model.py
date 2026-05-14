e5d177a6b205234996384031ae8c53a29949e5e5
AvalosRuben <rubenavalosmtz@gmail.com>
2026-05-13 20:56:54 -0600

    user_model

---
+++ app/models/user_model.py
@@ -0,0 +1,19 @@
+from typing import Optional
+from sqlmodel import SQLModel, Field
+
+class User(SQLModel, table=True):
+    __tablename__ = "users"
+    
+    id: Optional[int] = Field(
+        default=None,
+        primary_key=True
+    )
+
+    name:str
+
+    username: str = Field(
+        unique=True,
+        index=True
+    )
+
+    hashed_password:str
